f832db7a88e0e341fd90a3cf4f3162e2a43edfd1
hikhjin <hikhjin@gmail.com>
2023-11-01 16:00:02 +0900

    8단 변속기 수정

--- softeer/8단 변속기.py
+++ softeer/8단 변속기.py
@@ -1,13 +1,9 @@
 trans = list(map(int, input().split()))
 checkDe = [8, 7, 6, 5, 4, 3, 2, 1]
 checkAs = sorted(checkDe)
-for i in range(8):
-    if trans == checkDe:
+if trans == checkDe:
         print('descending')
-        break
-    elif trans == checkAs:
+elif trans == checkAs:
         print('ascending')
-        break
-    else:
-        print('mixed')
-        break
+else:
+    print('mixed')
