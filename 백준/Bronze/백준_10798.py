383bbcd7e612b8f5912bdc5e138751e2e338ac58
hikhjin <hikhjin@gmail.com>
2023-11-10 16:59:32 +0900

    백준_10798 코드 수정

--- 백준/Bronze/백준_10798.py
+++ 백준/Bronze/백준_10798.py
@@ -1,6 +1,7 @@
 import sys
 input = sys.stdin.readline
 
+'''
 arr = [list(str(input().rstrip())) for _ in range(5)]
 length = max(len(s) for s in arr) # 문자열 길이 최댓값
 
@@ -13,4 +14,14 @@ for i in range(len(arr)):
 for i in range(length):
     for j in range(len(arr)):
         ans += arr[j][i]
+print(ans)'''
+
+arr = [list(str(input().rstrip())) for _ in range(5)]
+ans = ''
+
+for i in range(15):
+    for j in range(5):
+        if i < len(arr[j]):
+            ans += arr[j][i]
+
 print(ans)
